35d50b0e77107cf9e84e810f45d3693e4f4aab65
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-10-16 15:01:06 +0530

    Cleaned backend routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -21,7 +21,7 @@ router.route('/').get((req, res) => {
 }); 
 
 /* Trigger the following if "http//www.website.com/users/register" is called */
-router.route('/signup').post((req, res) => {
+router.route('/register').post((req, res) => {
     /* A POST router that registers a new user */
     const name = req.body.name; 
     const email = req.body.email;
@@ -46,5 +46,129 @@ router.route('/signup').post((req, res) => {
         }); 
     }); 
 }); 
+
+/* Trigger the following if "http//www.website.com/users/login" is called */
+router.route('/login')
+    .post((req, res) => {
+        /* A POST route that logs-in an existing user */
+
+        /* Extracting values of email and raw password from the JSON object / HTML form */ 
+        const email = req.body.email;
+        const password = req.body.password;
+
+        
+        /* Look for the email in the database */
+        user.findOne({email})
+            
+            /* Do not confuse var "user" with var "_user". "user" refers to the local instance of the MongoDB Schema
+            while "_user" refers to the result of the findOne operation */ 
+            .then(_user => {
+                if (!_user)
+                    return res.status(400).json("Error: Email Not Found");   
+                
+                /* Do not confuse var "res" with var "_res". "res" refers to the response of the router
+                while "_res" refers to the response of the user entered password vs database hash comparison */
+                bcrypt.compare(password, _user.passwordHash, function(err, _res) {
+                    if (err){
+                        return res.json("Error: " + err.message); 
+                    }
+
+                    /* If the passwords match, store the users data in the session and redirect him to the landing page */ 
+                    if (_res) {
+                        req.session.user = _user; 
+                        return res.json(req.session.user); 
+                    }
+
+                    /* Else, return a negative response */
+                    else {
+                        return res.json("Passwords do not match"); 
+                    }
+                }); 
+            })
+            .catch(error => res.status(400).json("Error: " + error));  
+    });
+
+/* Trigger the following if "http//www.website.com/users/landing" is called */
+router.route('/profile')
+    
+    /* If the route is reached through a GET request */
+    .get((req, res) => {
+        /* A GET route triggered as the user information page. */
+        if (req.session.user) {
+
+            let resJSON = new Array(); 
+            resJSON.push(req.session.user);
+            
+            let email = req.session.user.email;
+
+            userSecondary
+                .findOne({email})
+                .then(_user => {
+ 
+                    if (!_user)         
+                        return res.json("Error: Email Not Found");   
+                    else {  
+                        resJSON.push(_user);
+                        console.log(resJSON);
+                        res.json(resJSON);
+                    }
+                })                      
+                .catch(error => res.status(400).json("Error: " + error));
+        }
+        else {
+            res.json("Not signed in!"); 
+        }
+    })
+
+    /* If the route is reached through a POST request */ 
+    .post((req, res) => {
+
+        /* Extract the name and email from json object/html form */
+        console.log(req.session.user); 
+        req.session.user.name = req.body.name; 
+        req.session.user.email = req.body.email; 
+
+        /* Hash the newly entered password */ 
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(req.body.passwordHash, salt, (err, hash) => {
+                
+                /* Extract the bare password from the html form/json object */
+                req.session.user.passwordHash = hash; 
+
+                /* Find the user id of the currently logged in user */
+                user.findById(req.session.user._id)
+                    
+                    /* If found, update the values */
+                    .then((_user) => {
+                        _user.passwordHash = hash; 
+                        _user.name = req.body.name;
+                        _user.email = req.body.email;
+                        _user.save(); 
+                    })
+                    
+                    /* And then send a json response with status */ 
+                    .then(_item => res.json(item))
+
+                    .catch(err => res.status(400).json("Error: " + err)); 
+            }); 
+        });
+    }); 
+
+
+/* Trigger the following if "http//www.website.com/users/landing" is called */
+router.route('/landing').get((req, res) => {
+    /* A GET route triggered as the user information page. */
+    
+    /* If there is a valid session in place, send back the data of the user */
+    if (req.session.user) {
+        res.json (req.session.user);
+    }
+
+    /* Otherwise, send a negative response */ 
+    else {
+        res.json(req.session.user); 
+    }
+});
+
 
 module.exports = router;
