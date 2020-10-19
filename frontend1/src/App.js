39ab70d36b87425ca04b4cd4ab305ec79509568e
Akshat-Singh <akshatsingh.hs@gmail.com>
2020-10-20 00:03:10 +0530

    Updated Cart.css. Added functional purchase component

--- frontend1/src/App.js
+++ frontend1/src/App.js
@@ -12,12 +12,11 @@ import UserWishlist from "./components/user-wishlist.component";
 import UserCart from "./components/user-cart.component";
 import UserSignout from "./components/user-signout.component"
 import Homepage from "./components/Homepage";
-
+import UserPurchases from "./components/user-purchases.component"; 
 
 import ProductsHome from "./components/products-home.component"; 
 import ProductsView from "./components/products-view.component";
 
-import Autocomplete from "./components/Autocomplete.js";
 
 axios.defaults.withCredentials = true; 
 
@@ -37,13 +36,14 @@ return (
 
           <Route path="/users/login" component={UserLogin}/>
           <Route path="/users/register" component={UserRegister}/>
+          <Route path="/users/profile/purchases" component={UserPurchases}/>
           <Route path="/users/profile" component={UserProfile}/>
           <Route path="/users/wishlist" component={UserWishlist}/>
           <Route path="/users/cart" component={UserCart}/>
           <Route path="/users/signout" component={UserSignout}/>
           <Route path="/products/:id" component={ProductsView}/> 
           <Route path = "/products" component={ProductsHome}/>    
-          <Route path="/testsearch" component={Autocomplete}> <Autocomplete /> </Route>
+          
           
           
           {/* ----------------------------------------------- */}
---
+++ frontend1/src/components/user-purchases.component.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'; 
+import axios from 'axios';
+import Header from '../Header'; 
+import Purchase from './Purchases.js'
+import './Purchases.css'
+
+axios.defaults.baseURL = "";
+axios.defaults.withCredentials = true; 
+
+export default class UserPurchases extends Component {
+
+    constructor(props) {
+        super(props); 
+
+        this.state = {
+            purchases: [], 
+            mdata: []
+        } 
+    }
+
+    componentWillMount() {
+        axios.get('http://localhost:5000/users/profile/purchases')
+            .then(res => {
+                console.log(res.data); 
+                this.setState({data: res.data});
+                let purchase_data = {"data": this.state.data}
+                axios.post('http://localhost:5000/shop/getProducts', purchase_data)
+                    .then(res_i => {
+                        console.log(res_i.data); 
+                        this.setState({mdata: res_i.data}); 
+                    }) 
+            })
+            .catch(err => console.log(err)); 
+    }    
+   
+    render() {
+        return (
+            <div class="purchases">
+                <Header/>
+                <h1>My Purchases</h1>
+                <div>
+                    <table>
+                        {this.state.mdata.map((data, index) => ( 
+                            <Purchase 
+                                title = {data['itemName']}
+                                description = {data.description}
+                                price = {data.cost}
+                                image = {data['imageLink']}
+                                category = {data.category}
+                                id = {data._id}
+                            />
+                        ))}
+                    </table>
+                </div>
+            </div> 
+        )
+    }
+}
