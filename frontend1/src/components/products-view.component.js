66e7467cecef7b2e40f6c6b71e35e6be5acaab26
XSiege999 <nik.3731@gmail.com>
2020-10-20 05:39:47 +0530

    Finished styling product view page

--- frontend1/src/components/products-view.component.js
+++ frontend1/src/components/products-view.component.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Header from "../Header";
 import Product from '../Product.js';
-import '../Product.css';
-import '../App.css'; 
+// import '../Product.css';
+// import '../App.css'; 
+import '../components/product-view.component.css'
 
 export default class ProductsView extends Component{
         
@@ -48,57 +49,61 @@ export default class ProductsView extends Component{
             <div> 
 
                 <Header />
-                
-                <div className="homepage">
-                    <div>
-                        <Product 
-                            title = {this.state.mdata['itemName']}
-                            description = {this.state.mdata.description}
-                            price = {this.state.mdata.cost}
-                            reviews = {this.state.mdata.totalRatings}
-                            rating = {this.state.mdata.avgRatings}
-                            image = {this.state.mdata['imageLink']}
-                            category = {this.state.mdata.category}
-                            id = {this.state.mdata._id}
-                        />
+                <div className="productViewWrapper">
+
+                    <div className="productActual">
+                        <div>
+                            <Product 
+                                title = {this.state.mdata['itemName']}
+                                description = {this.state.mdata.description}
+                                price = {this.state.mdata.cost}
+                                reviews = {this.state.mdata.totalRatings}
+                                rating = {this.state.mdata.avgRatings}
+                                image = {this.state.mdata['imageLink']}
+                                category = {this.state.mdata.category}
+                                id = {this.state.mdata._id}
+                            />
+                        </div>
                     </div>
-                    <div style={{color: "white"}}>
-                    <h1> Individual Ratings </h1> 
-                    <table>
-                        {this.state.ratingsArray.map((data, index) => (
-                            <tr>
-                                <td>{data.email}</td>
-                                <td>{data.rating}</td>
-                            </tr> 
-                        ))}
-                    </table>
-                </div>            
-                </div>
-                <div className = "login">
-                    <div className = "login__container">
-                        <h3> Average Rating: {this.state.mdata['avgRatings']} </h3>
+                      
+
+                    <div className="ratings"/*style={{color: "blue"}}*/>
+                        <h1>Here's how others rated this product:</h1> 
+                        <table>
+                            {this.state.ratingsArray.map((data, index) => (
+                                <tr>
+                                    <td>{data.email}:&nbsp;  &nbsp;</td>
+                                    <td className="ratingNum">&nbsp;&nbsp;{data.rating}/5</td>
+                                </tr> 
+                            ))}
+                        </table>
+                    </div>            
+                    
+
+                    <div className = "review">
+                        <div className = "review__container">
+                            <h3> Average Rating: {this.state.mdata['avgRatings']}/5 </h3>
+                            
+                            <h3> Total Reviews: {this.state.mdata['totalRatings']}  </h3>
                         
-                        <h3> Total Reviews: {this.state.mdata['totalRatings']}  </h3>
-                    
 
-                    
-                        Your rating
-                        <form onSubmit={this.submitRating}>
-                            <select className = "login__submit" onChange={this.onChangeRating}>
-                                <option value="0" selected={true} disabled={true}>0</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select>
-                            <button className = "login__submit" type="submit">Submit Rating</button>
-                        </form> 
+                        
+                            <p>Your rating:</p>
+                            <form onSubmit={this.submitRating}>
+                                <select className = "__submit" onChange={this.onChangeRating}>
+                                    <option value="0" selected={true} disabled={true}>0</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select>
+                                <button className = "login__submit" type="submit">Submit Rating</button>
+                            </form> 
 
-                    </div>
-
-                </div>      
-
+                        </div>
+                    </div>      
+                </div>
             </div>
         )
     }
